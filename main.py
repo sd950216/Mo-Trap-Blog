8e15fdf046d56b3493e81cefd34889e40cf9a94c
sd950216 <meen79508@gmail.com>
2022-08-18 06:55:37 +0200

    fixed contact me for none registered users

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ login_manager = LoginManager()
 login_manager.init_app(app)
 
 # CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URI'].replace('postgres://', 'postgresql://')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
