8c188283f95abbcd180d75d8c7183446229441b0
sd950216 <meen79508@gmail.com>
2022-08-24 20:20:50 +0200

    fix empty comment error

--- forms.py
+++ forms.py
@@ -23,7 +23,7 @@ class RegisterForm(FlaskForm):
 class LoginForm(FlaskForm):
     email = StringField("Email", validators=[DataRequired()])
     password = PasswordField("Password", validators=[DataRequired()])
-    submit = SubmitField("Register")
+    submit = SubmitField("Log In")
 
 
 class CommentForm(FlaskForm):
